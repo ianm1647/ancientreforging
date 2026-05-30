730009b9f3188b1479bff34614719022931cbb9e
ianmc <ianmcg1128@gmail.com>
2026-05-30 15:53:25 -0500

    26 .1 port

--- src/main/java/ianm1647/ancientreforging/screen/AncientReforgingScreen.java
+++ src/main/java/ianm1647/ancientreforging/screen/AncientReforgingScreen.java
@@ -104,7 +104,7 @@ public class AncientReforgingScreen extends AdventureContainerScreen<AncientRefo
 
         if (!hadItem && this.hasMainItem) {
             this.animationTick = MAX_ANIMATION_TIME;
-            this.minecraft.getSoundManager().play(SimpleSoundInstance.forUI(Apoth.Sounds.REFORGE, 1F, 2F));
+            this.minecraft.getSoundManager().play(SimpleSoundInstance.forUI(Apoth.Sounds.REFORGE_ITEM_REFORGED, 1F, 2F));
         }
 
         if (this.hasMainItem) {
